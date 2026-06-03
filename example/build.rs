8e0be79fb0c9754308cdbcdcff7ef481e80edea0
sheng <root@ltpp.vip>
2026-06-03 20:03:34 +0800

    feat: v0.5.24

--- example/build.rs
+++ example/build.rs
@@ -1,14 +1,19 @@
-use std::{env, fs::read_to_string, path::PathBuf};
+use std::{env, fs, fs::read_to_string, path::PathBuf};
 
 use {chrono::Local, toml::Table};
 
 const ENV_KEY_EUV_PACKAGE_NAME_KEY: &str = "EUV_PACKAGE_NAME";
 const ENV_KEY_EUV_VERSION_KEY: &str = "EUV_VERSION";
 const ENV_KEY_EUV_BUILD_TIME_KEY: &str = "EUV_BUILD_TIME";
+const BUILD_STATE_FILE_NAME: &str = ".euv_build_state";
 
 fn main() {
     let manifest_dir: String = env!("CARGO_MANIFEST_DIR").to_string();
-    let toml_path: PathBuf = PathBuf::from(manifest_dir).join("Cargo.toml");
+    let state_file_path: PathBuf = PathBuf::from(&manifest_dir).join(BUILD_STATE_FILE_NAME);
+    let state_file_path_display: std::path::Display<'_> = state_file_path.display();
+    println!("cargo:rerun-if-changed={state_file_path_display}");
+    let _ = fs::remove_file(&state_file_path);
+    let toml_path: PathBuf = PathBuf::from(&manifest_dir).join("Cargo.toml");
     let toml_content: String = read_to_string(&toml_path).expect("Failed to read Cargo.toml");
     let toml_table: Table = toml_content
         .parse::<Table>()
@@ -29,6 +34,7 @@ fn main() {
         .as_str()
         .expect("Version is not a string");
     let build_time_formatted: String = format!("{}", Local::now().format("%Y-%m-%d %H:%M:%S%.6f"));
+    fs::write(&state_file_path, &build_time_formatted).expect("Failed to write build state file");
     println!("cargo:rustc-env={ENV_KEY_EUV_PACKAGE_NAME_KEY}={package_name}");
     println!("cargo:rustc-env={ENV_KEY_EUV_VERSION_KEY}={version_value}");
     println!("cargo:rustc-env={ENV_KEY_EUV_BUILD_TIME_KEY}={build_time_formatted}");
